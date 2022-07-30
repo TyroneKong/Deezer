7c06271f40df691ce1b7048f294d955b7b846d4f
TyroneKong <tyronekong@gmail.com>
2022-07-30 14:03:26 +0100

    added controller

--- server/routes/search.js
+++ server/routes/search.js
@@ -2,25 +2,10 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 require("dotenv").config();
+const { getArtist } = require("../controllers/artistController");
 
 // search for artist
 
-router.get("/artist/:name", async (req, res) => {
-  const options = {
-    method: "GET",
-    url: "https://deezerdevs-deezer.p.rapidapi.com/search",
-    params: { q: req.params.name },
-    headers: {
-      "X-RapidAPI-Key": "2130c8f6c7msh1f292e2b2420a0ep1b215fjsn556d765ad938",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-    },
-  };
-  try {
-    const { data } = await axios.request(options);
-    res.status(200).json(data);
-  } catch (error) {
-    res.json(error);
-  }
-});
+router.get("/artist/:name", getArtist);
 
 module.exports = router;
